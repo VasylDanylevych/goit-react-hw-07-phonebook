5cb4b0fc1121e6a7128bbde6d7872c538268ca96
VasylDanylevych <ukrainec31@gmail.com>
2023-04-28 00:17:56 +0300

    upd

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
 import { reducer } from './reducer';
-
-// const persistConfig = {
-//   key: 'contactsList',
-//   storage: storage,
-//   whitelist: ['contacts'],
-// };
-
-// const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
   reducer: reducer,
 });
-
-export const persistor = persistStore(store);
